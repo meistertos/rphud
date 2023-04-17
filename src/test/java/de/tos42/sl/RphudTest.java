ad12e7a54f63d7d9772699f7f5b53abd90ce6df7
meistertos <meistertos>
2023-04-17 22:33:43 +0000

    Initial commit

---
+++ src/test/java/de/tos42/sl/RphudTest.java
@@ -0,0 +1,21 @@
+package de.tos42.sl;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class RphudTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
